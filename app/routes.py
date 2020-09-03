412f1a35a24ad31a4b063bbfd4f8ab3875eeac90
Andreas Stenberg <andreas@mewongu.com>
2020-09-03 11:41:23 +0200

    Created base design

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifer: MPL-2.0
 # Copyright © 2020 Andreas Stenberg
-
+from flask import render_template
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Hello world"
+    return render_template("index.html")
--- microapp.py
+++ microapp.py
@@ -4,5 +4,5 @@
 from app import app
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=80, debug=True) 
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=80, debug=True)
